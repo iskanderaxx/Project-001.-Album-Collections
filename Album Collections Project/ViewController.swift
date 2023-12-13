e66cf913aa317fa629bab4076cb7b714269fa8bc
Alexander Stepanov <alexanderlawyer97@gmail.com>
2023-12-13 19:28:32 +0100

    Initial Commit

---
+++ Album Collections Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Album Collections Project
+//
+//  Created by Mac Alexander on 13.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
